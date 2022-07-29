4f179572ee8abf640ace767422294fea7193bf7d
Laurentiuu <laurgales@yahoo.com>
2022-07-29 16:59:48 +0300

    imports

--- node/index.ts
+++ node/index.ts
@@ -1,84 +1,56 @@
-const crud = require("./controller/crud");
+import express from "express";
+import bodyParser from "body-parser";
 
-const express = require('express');
+import {Service} from "./controller/crud";
+
 const app = express();
 const PORT = 3000;
 const router = express.Router();
 
-const bodyParser = require("body-parser");
-const url = require('url');
-const queryString = require('querystring');
+const crud = new Service();
 
-crud.read()
+crud.read();
 
-app.get('/getFile', function(request: any, response: any) {
+app.get("/getFile", function (request: any, response: any) {
   response.send(crud.read());
-  
 });
 
-app.listen(PORT, function() {
-  console.log(`Example app listening on port ${PORT}!`)
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.post('/post',(request: any,response: any) => {
+router.post("/post", (request: any, response: any) => {
   //code to perform particular action.
   //To access POST variable use req.body()methods.
   // console.log(typeof(request.body))
   crud.add(request.body);
-  
+
   response.send("Succes adding");
 });
 
-router.delete('/delete',(request: any,response: any) => {
-  
-  const protocol = request.protocol;
-  const host = request.hostname;
-  const urlString = request.originalUrl;
-  const port = process.env.PORT || PORT;
-  //get url
-  const fullUrl = `${protocol}://${host}:${port}${urlString}`
-  const parsedUrl = url.parse(fullUrl);
-  //get querrystring
-  const parsedQs = queryString.parse(parsedUrl.query);
-  // crud.add(request.body);
-  const from = parsedQs.from;
+router.delete("/delete", (request: any, response: any) => {
+  const from = request.query.from;
+  console.log(from);
+
   crud.deleteData(from);
 
-  
   response.send("Succes Deleting");
 });
 
-router.put('/put',(request: any,response: any) => {
-  
-  const protocol = request.protocol;
-  const host = request.hostname;
-  const urlString = request.originalUrl;
-  const port = process.env.PORT || PORT;
-  //get url
-  const fullUrl = `${protocol}://${host}:${port}${urlString}`
-  const parsedUrl = url.parse(fullUrl);
-  //get querrystring
-  const parsedQs = queryString.parse(parsedUrl.query);
-  // crud.add(request.body);
-  const from = parsedQs.from;
-
+router.put("/put", (request: any, response: any) => {
   const reqBody = request.body;
 
-  crud.update(from, reqBody);
+  crud.update(request.query.from, reqBody);
 
-  
   response.send("Succes Putting");
 });
 
 // add router in the Express app.
 app.use("/", router);
-
-
-
 
 // crud.read();
 // crud.printData();
@@ -95,4 +67,3 @@ app.use("/", router);
 //     to: ["Tu"]
 // });
 // crud.deleteData('Eu');
-
